2d1855fbec06dea0eb1a6ff32402aa27ac261b5c
sharina-ya <uliasarina7@gmail.com>
2025-04-10 01:01:44 +1000

    Update urls.py

--- boards/urls.py
+++ boards/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('boards/<uuid:board_id>/delete/', views.delete_board, name='delete_board'),
     path('boards/<int:board_id>/', views.board_view, name='board_view'),
     path('<uuid:board_id>/pdf/', views.show_pdf, name='show_pdf'),
+    path('<uuid:board_id>/save/', views.save_board, name='save_board'),
+
 ]
