2b64ac893e8f1d592b07ee10ff2e4bf774c9e904
sharina-ya <uliasarina7@gmail.com>
2025-04-09 20:48:57 +1000

    Update urls.py

--- boards/urls.py
+++ boards/urls.py
@@ -4,9 +4,10 @@ from . import views
 app_name = 'boards'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('boards.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.home, name='home'),
+    path('create/', views.create_board, name='create_board'),
+    path('boards/<uuid:board_id>/', views.board_view, name='boards'),
+    path('boards/<uuid:board_id>/delete/', views.delete_board, name='delete_board'),
+    path('boards/<int:board_id>/', views.board_view, name='board_view'),
+    path('<uuid:board_id>/pdf/', views.show_pdf, name='show_pdf'),
+]
