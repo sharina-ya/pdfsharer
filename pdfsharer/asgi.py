ff0f75541786ed5ee6e2316d2275fbd32dea2c62
sharina-ya <uliasarina7@gmail.com>
2025-04-11 01:57:52 +1000

    Update asgi.py

--- pdfsharer/asgi.py
+++ pdfsharer/asgi.py
@@ -1,17 +1,7 @@
-"""
-ASGI config for pdfsharer project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from pdfsharer.boards import routing
+from boards import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdfsharer.settings')
 
