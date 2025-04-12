2a5966f56876fb6607ca5eeef2a15e357d40cf21
sharina-ya <uliasarina7@gmail.com>
2025-04-12 18:00:03 +1000

    Update routing.py

--- boards/routing.py
+++ boards/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/boards/(?P<board_id>\w+)/$', consumers.BoardConsumer.as_asgi()),
+    re_path(r'ws/board/(?P<board_id>\w+)/$', consumers.BoardConsumer.as_asgi()),
 ]
